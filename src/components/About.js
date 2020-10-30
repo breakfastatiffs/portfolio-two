2f000de2f6faced7dd8698abbe0585f1e2170ff5
Tiff Joy <tiffanysummerford@gmail.com>
2020-10-30 15:35:05 -0400

    fixed typo and line height

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
                 As a retired bartender with a "minor in marketing", I hold the skills to communicate effectively with a team, the creativity to design apps and the wit to absolve problems quickly. 
                 Working in restaurants taught me a lot about how the world works and I account that experience to why I am the way I am. 
                 I’ve always been more interested in working the front end of things, but I believe having an understanding of the entire house is key. 
-                Hence my interest and education as a full-stack developer. If you're intersted in learning more about me, let's video chat over some hot coffee.
+                Hence my interest and education as a full-stack developer. If you're interested in learning more about me, let's video chat over some hot coffee.
                 </p>
         </body>
     )
