b4a21870404b3944fc3df73911d6637184f2d91c
Tiff Joy <tiffanysummerford@gmail.com>
2020-11-03 15:09:31 -0500

    'updated'

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,12 @@ export default function About() {
     return (
         <section className='about-sect'>
             <img src={selfie} alt='self potrait of Tiffany Summerford' className='selfie'/>
-            <p className='body-p'>My names Tiffany Summerford and I'm a full-stack engineer from the Greater Boston area. 
-                As a retired bartender with a "minor in marketing", I hold the skills to communicate effectively with a team, the creativity to design apps and the wit to absolve problems quickly. 
-                Working in restaurants taught me a lot about how the world works and I account that experience to why I am the way I am. 
-                I’ve always been more interested in working the front end of things, but I believe having an understanding of the entire house is key. 
-                Hence my interest and education as a full-stack developer. If you're interested in learning more about me, let's video chat over some hot coffee.
+            <p className='body-p'>My name is Tiffany Summerford and I'm a full-stack engineer from the Greater Boston area. 
+            As a retired bartender with a "minor in marketing", I hold the skills to communicate effectively with a team, the creativity to design apps, 
+            and the wit to absolve problems quickly. Working in restaurants taught me a lot about how the world works and I account for that experience as to 
+            why I am the way I am. I’ve always been more interested in working on the front end of things, but I believe having an understanding of the entire
+             house is key. Hence my interest and education as a full-stack developer. If you're interested in learning more about me, let's video chat over 
+             some hot coffee.
                 </p>
         </section>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,7 +16,7 @@ export default function Projects() {
                 <a href='https://github.com/breakfastatiffs/nh-resource-client' className='fa fa-github-alt' target='_blank'> GitHub Client</a>
                 <a href='https://github.com/breakfastatiffs/nh-resources-server' className='fa fa-github-alt' target='_blank'> GitHub Server</a>
                 <hr className='style' />
-                <p>Altergencies is a compilation of emergency and non-emergency contacts for NH. By clicking a resource (business name), a user will be directed to the business's resource page which houses click-through links to call the business, access the bussiness's website, and even their social-media pages (if applicable). Users can view the list of resources, view by ID and are able to add resources, edit existing resources, and delete resources.</p>
+                <p>Altergencies is a compilation of emergency and non-emergency contacts for NH. By clicking a resource (business name), a user will be directed to the business's resource page which houses click-through links to call the business, access the business's website, and even their social media pages (if applicable). Users can view the list of resources, view by ID, and are able to add resources, edit existing resources, and delete resources.</p>
                 <p>FrontEnd: JavaScript, React, CSS 3, HTML 5, and Jest.</p>
                 <p>BackEnd: Node.js, Express.js, PostgreSQL, and Mocha & Chai.</p>
             </div>
@@ -29,7 +29,7 @@ export default function Projects() {
                 <a href='https://github.com/EndlessEncounters/client' className='fa fa-github-alt' target='_blank'> GitHub Client</a>
                 <a href='https://github.com/EndlessEncounters/server' className='fa fa-github-alt' target='_blank'> GitHub Server</a>
                 <hr className='style' />
-                <p>Endless Encounters is a text-based RPG ("Role Playing Game") developed by EE: John Pendergast, Alexis Felts, Scott Whiteman, and Tiffany Summerford. Once you're in, you can explore a whole new world in search of ravenous enemies to duel swords with or visit the tavern to discuss rumors. Endless Encounters includes randomly generated events and actions/abilities to respond with.</p>
+                <p>Endless Encounters is a text-based RPG ("Role-Playing Game") developed by EE: John Pendergast, Alexis Felts, Scott Whiteman, and Tiffany Summerford. Once you're in, you can explore a whole new world in search of ravenous enemies to duel swords with or visit the tavern to discuss rumors. Endless Encounters includes randomly generated events and actions/abilities to react with.</p>
                 <p>FrontEnd: JavaScript, React, CSS 3, HTML 5, and Jest.</p>
                 <p>BackEnd: Node.js, Express.js, PostgreSQL, and Mocha & Chai.</p>
             </div>
@@ -42,7 +42,7 @@ export default function Projects() {
                 <a href='https://github.com/thinkful-ei-quail/SR-Client-johnb-tiff' className='fa fa-github-alt' target='_blank'> GitHub Client</a>
                 <a href='https://github.com/thinkful-ei-quail/SR-API-johnb-tiff' className='fa fa-github-alt' target='_blank'> GitHub Server</a>
                 <hr className='style' />
-                <p>Spaced repetition is an advanced learning technique that enhances ones learning through an algorithm based upon repetition. The learner is quizzed on a sequence of words and when the learner answers incorrectly, they will see the word within a shorter time period versus if they answer correctly, they will see the word in a longer time period. With the number of Spanish speakers on the rise and halloween right around the corner, Spanish SRS was built with amusement and utility in mind.</p>
+                <p>Spaced repetition is an advanced learning technique that enhances ones learning through an algorithm based upon repetition. The learner is quizzed on a sequence of words and when the learner answers incorrectly, they will see the word within a shorter period versus if they answer correctly, they will see the word in a longer period. With the number of Spanish speakers on the rise and Halloween right around the corner, Spanish SRS was built with amusement and utility in mind.</p>
                 <p>Front End: React, JavaScript, Cypress, CSS3, HTML5, Vercel</p>
                 <p>Back End: Express, JavaScript, PostgreSQL, Heroku, Mocha & Chai</p>
             </div>
