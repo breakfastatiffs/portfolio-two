902e5932ec2fdd0b686cf037b3e0262a3656b165
Tiff Joy <tiffanysummerford@gmail.com>
2020-11-04 17:27:21 -0500

    css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
 import App from './components/App';
 import './index.css';
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<div className='app'><App /></div>, document.getElementById('root'));
